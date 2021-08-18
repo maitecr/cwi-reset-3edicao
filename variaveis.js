cd1873fbd26ed51e375599c09b19fe57793c99e8
Maitê <81782090+maitecr@users.noreply.github.com>
2021-08-18 10:59:22 -0300

    Create variaveis.js
    Primeiro exercício, sobre variáveis, do módulo de Lógica de Programação, pelo programa Reset da empresa CWI.

---
+++ variaveis.js
@@ -0,0 +1,11 @@
+nomeCompleto = "Maitê Capistrano Refosco";
+apelido = "Mai";
+idade = 28;
+nascimento = "06 de maio";
+localNasci = "Gravataí";
+altura = 1.67;
+trabalhando = false;
+
+apresentacao = "Meu nome é " + nomeCompleto + " e sou conhecida como " + apelido + " e tenho " + idade + " anos. Nasci no dia " + nascimento + ", na cidade de " + localNasci + ". Tenho " + altura + "m de altura e atualmente estou " + (trabalhando ? "empregado" : "desempregado") + ".";
+
+console.log(apresentacao)
